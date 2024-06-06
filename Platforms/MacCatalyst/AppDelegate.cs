7855ba7ddaa380563db2730884a63d7441e9fac1
laboratorio <laboratorio@UPOAULA10616>
2024-06-06 07:22:25 -0500

    Agregar archivos de proyecto.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JorgeNegrete
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
